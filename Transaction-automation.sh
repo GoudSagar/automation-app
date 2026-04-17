80ae0b71550c8f75eabfeb9dddcff6256114f29f
GoudSagar <goud.sagar.t@gmail.com>
2026-04-17 02:55:24 +0000

    Ticket-76543 Updated Transaction-automation script

--- Transaction-automation.sh
+++ Transaction-automation.sh
@@ -1,1 +1,20 @@
 #TO DO
+
+----------------------------------------------------------------------------------------------
+[root@ip-172-31-46-248 automation-devops]# git fetch origin develop
+From https://github.com/devopscloudtrainer25/automation-devops
+ * branch            develop    -> FETCH_HEAD
+[root@ip-172-31-46-248 automation-devops]# git checkout -b develop origin/develop
+branch 'develop' set up to track 'origin/develop'.
+Switched to a new branch 'develop'
+[root@ip-172-31-46-248 automation-devops]# git branch
+* develop
+  feature/test-work
+  main
+  release
+[root@ip-172-31-46-248 automation-devops]# 
+=========================================================================================================================
+* Remotes
+* Pull Request
+* Fork Mechanism
+======================================================
